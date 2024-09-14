2bd69748877ed64149186846959a69fdfb2be388
webbag <139613375+webbag1@users.noreply.github.com>
2024-09-14 12:18:13 +0530

    Add files via upload

---
+++ section4.js
@@ -0,0 +1,36 @@
+let num = document.getElementById("counter");
+let count = 0;
+let target = 126;
+let section4 = document.getElementById("section4");
+let line1 = document.getElementById("line1");
+let line2 = document.getElementById("line2");
+let line3 = document.getElementById("line3");
+let cover = document.getElementById("cover");
+
+function updateCounter() {
+      if (count <= target) {
+        num.textContent = count.toString().padStart(3, '0');
+        count++;
+        requestAnimationFrame(updateCounter);
+      }
+    }
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY >= section4.offsetTop - 700){
+    // count = 0
+    
+    line1.classList.add("slide-in-right")
+    line2.classList.add("slide-in-left")
+    line3.classList.add("slide-in-right")
+    setTimeout(() => {
+      updateCounter(); 
+      cover.classList.add("scale-out-hor-right")
+    }, 3000);  
+  } else {
+    // cover.classList.remove("scale-out-hor-right")
+    line1.classList.remove("slide-in-right")
+    line2.classList.remove("slide-in-left")
+    line3.classList.remove("slide-in-right")
+}
+})
+console.log(section4.offsetTop)
